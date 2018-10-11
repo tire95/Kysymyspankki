c77e20b7ea3a47836eaca0ab9955c8cf7e041fe6
Reijonen Timo T <tire@da5-cs-b221-02.pc.helsinki.fi>
2018-10-11 16:38:03 +0300

    testi

--- src/main/java/tikape/harjoitustyo2/Main.java
+++ src/main/java/tikape/harjoitustyo2/Main.java
@@ -9,7 +9,6 @@ package tikape.harjoitustyo2;
  *
  * @author tire
  */
-
 import java.sql.*;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -21,29 +20,34 @@ import tikape.harjoitystyo2.dao.KysymysDao;
 import tikape.harjoitystyo2.dao.VastausDao;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
+
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
+
         Database database = new Database("jdbc:sqlite:tietokanta.db");
         KysymysDao kysymysDao = new KysymysDao(database);
         VastausDao vastausDao = new VastausDao(database);
-        
+
         Spark.get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("kysymykset", kysymysDao.findAll());
-            
+
             return new ModelAndView(map, "aloitussivu");
         }, new ThymeleafTemplateEngine());
-        
-        
+
         Spark.post("/lisaaKysymys", (req, res) -> {
             String kurssinNimi = req.queryParams("kurssinNimi");
             String aihe = req.queryParams("aihe");
             String kysymysteksti = req.queryParams("kysymysteksti");
-            
+
             kysymysDao.saveOrUpdate(new Kysymys(-1, kurssinNimi, aihe, kysymysteksti));
             res.redirect("*");
             return "";
         });
-        
+
         Spark.get("/kysymys/:id", (req, res) -> {
             HashMap map = new HashMap<>();
             Integer kysymysId = Integer.parseInt(req.params(":id"));
@@ -54,5 +58,4 @@ public class Main {
         }, new ThymeleafTemplateEngine());
     }
 
-    
 }
